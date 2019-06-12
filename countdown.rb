18652ace198a11a45d492972416c32d459af08c2
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-12 19:28:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,20 @@
 #write your code here
 
 def countdown(numbers)
-  count=0
+  numbers=0
   while count > numbers
   puts "#{count} SECOND(S)!"
   count -= 1
 end
 puts "HAPPY NEW YEAR!"
+end
+def countdown(num)
+
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  num -= 1
+
+ end
+  "HAPPY NEW YEAR!"
 end
